a6a7465dd1a15ce92fec67751ec6a141a0d1faac
Steve Ohashi <steve@junkpiano.me>
2018-06-22 01:22:21 +0900

    delete key path.

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-rsync -avuze "ssh -i ~/.ssh/linode" --delete _site yusuke-bot@$1:~/
-ssh -ti ~/.ssh/linode yusuke-bot@$1 "sudo rsync -avuz --delete _site/ /var/www/$2/ && rm -rf _site"
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site yusuke-bot@$1:~/
+ssh -t yusuke-bot@$1 "sudo rsync -avuz --delete _site/ /var/www/$2/ && rm -rf _site"
