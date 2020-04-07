4328ad1c05c91c834189b459ea76214dcaf38ac5
Yusuke Ohashi <github@junkpiano.me>
2020-04-07 19:38:10 +0900

    Update blog

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git push origin master
 
 echo "publish on github page..."
 
-rm -rf $current_dir/public
+mv $current_dir/public $current_dir/public.bak
 git clone https://github.com/junkpiano/junkpiano.github.io.git $current_dir/public 
 hugo --gc --minify --config config_github.toml
 cd $current_dir/public
@@ -19,3 +19,8 @@ git commit -m "Update blog"
 git push origin master
 
 cd $current_dir
+
+git checkout -- .
+git clean -fd
+rm -rf $current_dir/public
+mv $current_dir/public.bak $current_dir/public
